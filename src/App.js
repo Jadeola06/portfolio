b78a5edb8f4b0571c4e4e2ee8d4df9de7c730b48
Jadeola06 <114201323+Jadeola06@users.noreply.github.com>
2024-01-27 10:52:23 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Credits from './pages/Credits'
+
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/credits" element={<Credits />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
