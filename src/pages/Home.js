738c6537bfbbc7646b224e3b24c72935c871cc3c
Jadeola06 <114201323+Jadeola06@users.noreply.github.com>
2025-03-12 13:10:02 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,65 +1,94 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProjectCard from "../components/ProjectCard.js";
-import axios from 'axios';
+import axios from "axios";
+import { motion } from "framer-motion";
 
 export default function Home() {
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://18.191.202.13:8080/api/v1/client', {
-                    withCredentials: true,
-                });
-                console.log(response.data);
-            } catch (error) {
-                console.error('There was an error fetching the members!', error);
-            }
-        };
-
-        fetchData(); // Call fetchData inside useEffect
-    }, []); // Empty dependency array ensures this runs once when the component mounts
-
     return (
         <div className="min-h-screen bg-gradient-to-b from-purple-100 to-blue-100">
             <div className="container mx-auto p-8">
-                <div className="text-center mb-16">
+                {/* About Me Section */}
+                <motion.div
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                    className="text-center mb-16"
+                >
                     <h1 className="text-5xl font-bold text-purple-800">Joseph Adeola</h1>
                     <h4 className="text-2xl text-blue-600 mt-2">Full Stack Software Engineer</h4>
-                </div>
+                    <p className="text-lg text-gray-700 mt-4">
+                        I am a passionate full-stack developer with experience in building scalable and efficient applications.
+                        I specialize in both front-end and back-end development, using technologies like React, Spring Boot, and more.
+                    </p>
+                </motion.div>
 
-                <div className="mb-16">
+                {/* Skills Section */}
+                <motion.div
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                    className="mb-16"
+                >
                     <h2 className="text-3xl font-semibold text-purple-800 mb-8">Skills</h2>
                     <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
-                        {['HTML, CSS, JavaScript', 'React JS', 'Flutter', 'SpringBoot', 'RESTful API design', 'SQL', 'Terraform', 'Docker', 'Kubernetes', 'Jenkins', 'AWS', 'Azure'].map(skill => (
-                            <div key={skill} className="bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold text-center p-4 rounded-lg shadow-lg hover:from-blue-500 hover:to-purple-500 transition-colors duration-300">
+                        {['React', 'SpringBoot', 'JavaScript', 'Flutter', 'AWS', 'Docker', 'SQL', 'Kubernetes'].map(skill => (
+                            <div key={skill} className="bg-gradient-to-r from-purple-500 to-blue-500 text-white font-semibold text-center p-4 rounded-lg shadow-lg hover:from-blue-500 hover:to-purple-500 transition-colors duration-300 transform hover:scale-105">
                                 {skill}
                             </div>
                         ))}
                     </div>
-                </div>
+                </motion.div>
 
-                <div>
+                {/* Projects Section */}
+                <motion.div
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                >
                     <h2 className="text-3xl font-semibold text-purple-800 mb-8">Projects</h2>
                     <div className="grid gap-8 lg:grid-cols-2">
-                        <ProjectCard 
-                            title="Expense Tracker Application" 
-                            description="Manage your finances effortlessly with my Expense Tracker application. It's a simple tool designed to help you keep track of your expenses, set budget goals, and gain insights into your spending patterns. Stay organized and make informed financial decisions." 
-                            technologies="React.js, Springboot, CSS, RESTful API Design"
+                        <ProjectCard
+                            title="Expense Tracker Application"
+                            description="Manage your finances effortlessly with this app. Track expenses, set budget goals, and gain insights."
+                            technologies="React.js, Springboot, CSS, RESTful API"
                             image="https://static.sheetgo.com/wp-content/uploads/2020/05/icons-expense-tracker.svg"
+                            link="https://example.com/expense-tracker"
                         />
-                        <ProjectCard 
-                            title="Mobile Budgeting App" 
-                            description="A mobile app that helps to simplify your finances on the fly. Created with Flutter and Dart. Effortlessly track expenses and set spending limits."
-                            technologies="Flutter, SQL, Dart"
+                        <ProjectCard
+                            title="Mobile Budgeting App"
+                            description="A mobile app created with Flutter and Dart to help you track your finances."
+                            technologies="Flutter, Dart, SQL"
                             image="https://miro.medium.com/v2/resize:fit:4800/format:webp/1*PiEaWUoTxVFPU-Nr2k2J_Q.png"
+                            link="https://example.com/mobile-budgeting"
                         />
-                        <ProjectCard 
-                            title="Quiz Website" 
-                            description="A minimalist quiz website powered by React.js. Delve into a variety of subjects with questions sourced from an external API, ensuring fresh and diverse content."
+                        <ProjectCard
+                            title="Quiz Website"
+                            description="A minimalist quiz website powered by React.js with questions from an external API."
                             technologies="React.js, CSS, API Integration"
                             image="https://static.vecteezy.com/system/resources/previews/003/206/208/non_2x/quiz-time-neon-signs-style-text-free-vector.jpg"
+                            link="https://example.com/quiz-website"
                         />
                     </div>
-                </div>
+                </motion.div>
+
+                {/* Full Stack Projects Section */}
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                    className="mt-16"
+                >
+                    <h2 className="text-3xl font-semibold text-purple-800 mb-8">Deploying Full-Stack Projects</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                        <ProjectCard
+                            title="Full-Stack App"
+                            description="A deployed full-stack application with real-time data updates and user authentication."
+                            technologies="React.js, Node.js, MongoDB, AWS"
+                            image="https://image.shutterstock.com/image-vector/full-stack-development-concept-web-260nw-1509533706.jpg"
+                            link="https://example.com/full-stack-app"
+                        />
+                    </div>
+                </motion.div>
             </div>
         </div>
     );
